5f74ec7a199fbf89b8c18f9f5e0380353b531101
exeynod <exeynod@gmail.com>
2020-07-05 11:55:47 +0300

    codestyle fixes x2

--- SuperKrutoyDocumentooborot/settings.py
+++ SuperKrutoyDocumentooborot/settings.py
@@ -85,11 +85,8 @@ DATABASES = {
         'PORT': '',
     }
 }
-try:
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
-except:
-    pass
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
