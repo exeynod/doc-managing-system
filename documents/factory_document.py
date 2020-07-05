a0bb5cc8acb9c4ed5928ca840fda38918dbce4c8
exeynod <exeynod@gmail.com>
2020-07-05 13:39:37 +0300

    test added

--- documents/factory_document.py
+++ documents/factory_document.py
@@ -9,4 +9,5 @@ class Creator:
     def create(path_to_file, user_id, primary=False) -> Document:
         if path_to_file[len(path_to_file) - 4:] == '.pdf':
             return PDFDocument(user_id, path_to_file, primary)
+        raise ValueError('Invalid extension')
 
