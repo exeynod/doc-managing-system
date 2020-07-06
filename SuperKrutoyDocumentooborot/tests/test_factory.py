e51bd519f7f24c611fd6ee42037f46e8df36c1a6
exeynod <exeynod@gmail.com>
2020-07-06 10:11:01 +0300

    factory fixed + docx tests added + CI errors fixed

--- SuperKrutoyDocumentooborot/tests/test_factory.py
+++ SuperKrutoyDocumentooborot/tests/test_factory.py
@@ -3,10 +3,11 @@ from documents.document import Document
 from documents.factory_document import Creator
 
 
-@pytest.mark.parametrize('path', ('.pdf', '21837213.pdf', '.pdf.pdf'))
+@pytest.mark.parametrize('path', ('', '21837213', '.pdf'))
 def test_right_creation(path):
     path = f'./SuperKrutoyDocumentooborot/tests/test_docs/right/{path}'
-    assert isinstance(Creator.create(path, '1', True), Document)
+    assert isinstance(Creator.create(path + '.pdf', '1', True), Document)
+    assert isinstance(Creator.create(path + '.docx', '1', True), Document)
 
 
 @pytest.mark.parametrize('path', ('.doc', '.pdf21837213', '.pdf.doc'))
--- documents/factory_document.py
+++ documents/factory_document.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from documents.document import Document
 from documents.pdf_document import PDFDocument
+from documents.docx_document import DOCXDocument
 
 
 class Creator:
@@ -9,4 +10,6 @@ class Creator:
     def create(path_to_file, user_id, primary=False) -> Document:
         if path_to_file[len(path_to_file) - 4:] == '.pdf':
             return PDFDocument(user_id, path_to_file, primary)
+        if path_to_file[len(path_to_file) - 5:] == '.docx':
+            return DOCXDocument(user_id, path_to_file, primary)
         raise ValueError('Invalid extension')
