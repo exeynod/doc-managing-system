d16cd1486018d0a650691d9d2428163013f21a25
exeynod <exeynod@gmail.com>
2020-07-06 19:54:04 +0300

    logging file changed

--- web/views.py
+++ web/views.py
@@ -133,7 +133,7 @@ def add_new_document(request):
     import logging
     info_logger = logging.getLogger('Info_Logger')
     info_logger.setLevel(logging.INFO)
-    fh_info = logging.FileHandler('good_log.log', encoding='utf-8')
+    fh_info = logging.FileHandler('info_log.log', encoding='utf-8')
     fh_info.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fh_info.setFormatter(formatter)
