8cad18478d1aeb260f699031bb32c90c070f07a0
Sablin Sergey <serewkasablin@outlook.com>
2020-07-06 18:40:28 +0400

    Codestyle updates'

--- web/views.py
+++ web/views.py
@@ -246,8 +246,8 @@ def search(request):
     notifications = user.profile.get_notifications()
     text = request.POST.get('text')
     personal_context = user.profile.get_statistic()
-    files_found = [Document.objects.filter(filename=text + ext).
-                       filter(Q(owner__user__username=user.username) | Q(reviewer__user__username=user.username))
+    files_found = [Document.objects.filter(filename=text + ext).filter(Q(owner__user__username=user.username) |
+                                                                       Q(reviewer__user__username=user.username))
                    for ext in extensions]
     context = {'username': user.username, 'notifications': notifications, 'files_found': files_found}
     context.update(personal_context)
